237eaa078cb43ddb15f10301a2d1363cc73ae8b6
Andreas Hyldahl <Andreas.hyldahl10@hotmail.com>
2018-11-25 14:43:56 +0100

    Chancekort1

--- 33_del3/src/main/java/entity/Chance.java
+++ 33_del3/src/main/java/entity/Chance.java
@@ -6,4 +6,16 @@ public class Chance extends Field {
     }
     // dan objekter af hver chanceklasse og inddel dem i et array, som starter tilfældigt
     // herfra lægges øverste kort nederst i arrayet, så alle rykker en gang ned.
+
+    Account currentAccount;
+
+
+
+    public void chancecard1(){
+        System.out.println("Du har lavet alle dine lektier. Modtag 2 af banken");
+        currentAccount.addToScore(2);
+    }
 }
+
+
+// dan objekter fra Chance-klassen og kald de enkelte metoder for hvert kort.
