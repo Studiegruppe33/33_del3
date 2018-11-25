9a43965a3dc5017cadc6f41f0fc1e544f61ae835
SaschaThorsgaardJacobsen <s171281@student.dtu.dk>
2018-11-25 15:16:40 +0100

    Første forsøg på chancekort

---
+++ 33_del3/src/main/java/entity/ChanceCard.java
@@ -0,0 +1,31 @@
+package java.entity;
+
+public class ChanceCard extends Chance{
+    public ChanceCard(int number, String name, int result, int card) {
+        super(number, name, result);
+        this.card = card;
+    }
+
+    private int card;
+    Account currentAccount;
+
+    ChanceCard chancecard1;
+    ChanceCard chancecard2;
+    ChanceCard chancecard3;
+    ChanceCard chancecard4;
+    ChanceCard chancecard5;
+    ChanceCard chancecard6;
+    ChanceCard chancecard7;
+    ChanceCard chancecard8;
+    ChanceCard chancecard9;
+    ChanceCard chancecard10;
+
+    public void deck() {
+        Chance[] chanceDeck = new Chance[10];
+        Chance[0] = new ChanceCard()
+
+    }
+
+
+    }
+
