778b575338a8381823121f407237734b8675e41c
Peter Bach <s185035@student.dtu.com>
2018-11-28 12:49:52 +0100

    Rettelse af fejl i Chance

--- 33_del3/src/main/java/ent/Chance.java
+++ 33_del3/src/main/java/ent/Chance.java
@@ -6,6 +6,7 @@ public abstract class Chance extends Field {
     public Chance(int nummer) {
         super(1, "chance", 1);
     }
+
     Scanner scanner = new Scanner(System.in);
     // dan objekter af hver chanceklasse og inddel dem i et array, som starter tilfældigt
     // herfra lægges øverste kort nederst i arrayet, så alle rykker en gang ned.
@@ -14,29 +15,30 @@ public abstract class Chance extends Field {
     Player player;
     String chance;
 
-    public String toString(){
+    public String toString() {
         return "Du har landet på Chancen, tryk enter for at trække et kort.";
     }
 
-    public void setChanceConsequence(){
+    public void setChanceConsequence() {
         this.chance = ChanceCard.pullCard();
     }
 
-    public String getChanceConsequence(){
+    public String getChanceConsequence() {
         return chance.split("#")[1];
     }
 
     private String[] split(String s) {
-    return null; }
+        return null;
+    }
 
     public void landOnField(Player player) {
 
         String field;
         setChanceConsequence();
-        boolean newCard=true;
-        while(newCard){
+        boolean newCard = true;
+        while (newCard) {
             newCard = false;
-            switch (chance.split("#")[0]){
+            switch (chance.split("#")[0]) {
                 case "Chance1":
                     System.out.println("Du har lavet alle dine lektier. Modtag 2 af banken");
                     account.addToScore(2);
@@ -50,81 +52,83 @@ public abstract class Chance extends Field {
                     System.out.println("Ryk frem til et orange felt: Hvis det er ledigt, " +
                             "får du det gratis! Ellers skal du betale leje til ejeren ");
                     System.out.println("Tryk 1 for at vælge Skaterparken eller tryk 2 for Swimmingpoolen");
-                    while(true){
+                    while (true) {
                         System.out.println("Tryk 1 eller 2");
                         int fieldChoice = scanner.nextInt();
-                        if(fieldChoice == 1){
-                            player.setPosition(10);
-                        }else if(fieldChoice == 2){
-                            player.setPosition(11);
-                        }
-                    break;
-
-                case "Chance4":
-                    System.out.println("Du har spist for meget slik. Betal M2 til banken");
-                    account.addToScore(-2);
-                    break;
-
-                case "Chance5":
-                    System.out.println("Tryk 1 for at rykke 1 felt frem eller tryk 2 for at tage et chancekort mere");
-
-                    while (true) {
-                        System.out.println("Tryk 1 eller 2");
-                        fieldChoice = scanner.nextInt();
-                        if (fieldChoice == 1) {
-                            player.setPosition(player.getPosition() + 1);
-
-                        } else if (fieldChoice == 2) {
-                            player.setPosition(player.getPosition());
-                        }
-                    }
-                    break;
-
-                case "Chance6":
-                    System.out.println("Gratis Felt!");
-                    System.out.println("Ryk frem til et orange eller grønt felt: Hvis det er ledigt, " +
-                            "får du det gratis! Ellers skal du betale leje til ejeren ");
-                    System.out.println("Tryk 1 for at vælge Skaterparken, tryk 2 for Swimmingpoolen, tryk 3 for Bowlinghallen eller tryk 4 for Zoologiskhave ");
-                    while (true) {
-                        System.out.println("Tryk 1,2,3 eller 4");
-                        fieldChoice = scanner.nextInt();
                         if (fieldChoice == 1) {
                             player.setPosition(10);
                         } else if (fieldChoice == 2) {
                             player.setPosition(11);
-                        } else if (fieldChoice == 3) {
-                            player.setPosition(19);
-                        } else if (fieldChoice == 4) {
-                            player.setPosition(20);
                         }
-
                     }
+                        break;
+
+                        case "Chance4":
+                            System.out.println("Du har spist for meget slik. Betal M2 til banken");
+                            account.addToScore(-2);
+                            break;
+
+                        case "Chance5":
+                            System.out.println("Tryk 1 for at rykke 1 felt frem eller tryk 2 for at tage et chancekort mere");
+
+                            while (true) {
+                                System.out.println("Tryk 1 eller 2");
+                                int fieldChoice = scanner.nextInt();
+                                if (fieldChoice == 1) {
+                                    player.setPosition(player.getPosition() + 1);
+
+                                } else if (fieldChoice == 2) {
+                                    player.setPosition(player.getPosition());
+                                }
+                            }
+                            break;
+
+                        case "Chance6":
+                            System.out.println("Gratis Felt!");
+                            System.out.println("Ryk frem til et orange eller grønt felt: Hvis det er ledigt, " +
+                                    "får du det gratis! Ellers skal du betale leje til ejeren ");
+                            System.out.println("Tryk 1 for at vælge Skaterparken, tryk 2 for Swimmingpoolen, tryk 3 for Bowlinghallen eller tryk 4 for Zoologiskhave ");
+                            while (true) {
+                                System.out.println("Tryk 1,2,3 eller 4");
+                                int fieldChoice = scanner.nextInt();
+                                if (fieldChoice == 1) {
+                                    player.setPosition(10);
+                                } else if (fieldChoice == 2) {
+                                    player.setPosition(11);
+                                } else if (fieldChoice == 3) {
+                                    player.setPosition(19);
+                                } else if (fieldChoice == 4) {
+                                    player.setPosition(20);
+                                }
+
+                            }
+                            break;
+
+                        case "Chance7":
+                            System.out.println("Gratis Felt!");
+                            System.out.println("Ryk frem til et lyseblåt felt: Hvis det er ledigt, " +
+                                    "får du det gratis! Ellers skal du betale leje til ejeren ");
+                            System.out.println("Tryk 1 for at vælge Slikbutikken eller tryk 2 for Iskiosken");
+                            while (true) {
+                                System.out.println("Tryk 1 eller 2");
+                                int fieldChoice = scanner.nextInt();
+                                if (fieldChoice == 1) {
+                                    player.setPosition(5);
+                                } else if (fieldChoice == 2) {
+                                    player.setPosition(6);
+                                }
+
+                            }
+                            break;
+
+                        case "Chance8":
+                            System.out.println("Ryk frem til Strandpromenaden");
+                            player.setPosition(24);
+
                     break;
 
-                case "Chance7":
+                case "Chance9": {
                     System.out.println("Gratis Felt!");
-                    System.out.println("Ryk frem til et lyseblåt felt: Hvis det er ledigt, " +
-                            "får du det gratis! Ellers skal du betale leje til ejeren ");
-                    System.out.println("Tryk 1 for at vælge Slikbutikken eller tryk 2 for Iskiosken");
-                    while(true){
-                        System.out.println("Tryk 1 eller 2");
-                        int fieldChoice = scanner.nextInt();
-                        if(fieldChoice == 1){
-                            player.setPosition(5);
-                        }else if(fieldChoice == 2){
-                            player.setPosition(6);
-                        }
-
-                    }
-                    break;
-
-                case "Chance8":
-                    System.out.println("Ryk frem til Strandpromenaden");
-                    player.setPosition(24); }
-                    break;
-
-                case "Chance9":
-                { System.out.println("Gratis Felt!");
                     System.out.println("Ryk frem til et pink eller mørkeblåt felt: Hvis det er ledigt, " +
                             "får du det gratis! Ellers skal du betale leje til ejeren ");
                     System.out.println("Tryk 1 for at vælge Museet, tryk 2 for Biblioteket, tryk 3 for Vandlandet eller tryk 4 for Strandpromenaden ");
@@ -142,19 +146,24 @@ public abstract class Chance extends Field {
                         }
 
                     }
+                }
                     break;
 
-                case "Chance10":
-                    System.out.println("Du har lavet alle dine lektier. Modtag 2 af banken");
-                    account.addToScore(2);
-                    break;
+                    case "Chance10":
+                        System.out.println("Du har lavet alle dine lektier. Modtag 2 af banken");
+                        account.addToScore(2);
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
+                }
             }
         }
     }
 }
+
+
+
 /*
 
     public void chancecard1() {
