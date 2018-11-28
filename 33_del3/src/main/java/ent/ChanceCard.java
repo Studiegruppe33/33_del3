7eaf4e2869269ff8f780936261fe65cc40424dd0
SaschaThorsgaardJacobsen <s171281@student.dtu.dk>
2018-11-28 12:42:44 +0100

    Sascha chancekort forsøg

--- 33_del3/src/main/java/ent/ChanceCard.java
+++ 33_del3/src/main/java/ent/ChanceCard.java
@@ -6,81 +6,55 @@ import java.util.Random;
     public ChanceCard(int number, String name, int result, int card) {
         super(number, name, result);
         this.card = card;
+    }*/
+
+public class ChanceCard {
+
+
+    private  String[] chancecard;
+
+    public void Chance() {
+        chancecard = new String[10];
+        chancecard[0] = "Chance 1";
+        chancecard[1] = "Chance 2";
+        chancecard[2] = "Chance 3";
+        chancecard[3] = "Chance 4";
+        chancecard[4] = "Chance 5";
+        chancecard[5] = "Chance 6";
+        chancecard[6] = "Chance 7";
+        chancecard[7] = "Chance 8";
+        chancecard[8] = "Chance 9";
+        chancecard[9] = "Chance 10";
+
     }
 
-    private int card;
-    Account currentAccount;
-
-    ChanceCard chancecard1;
-    ChanceCard chancecard2;
-    ChanceCard chancecard3;
-    ChanceCard chancecard4;
-    ChanceCard chancecard5;
-    ChanceCard chancecard6;
-    ChanceCard chancecard7;
-    ChanceCard chancecard8;
-    ChanceCard chancecard9;
-    ChanceCard chancecard10;
-
-*/
-
-    public class ChanceCard {
-
-        Chance chance;
-        private int number;
-        Chance[] chanceDeck = new Chance[10];
-        public ChanceCard(int number){
-        this.number = number;
+    public Field pullCard() {
+        String pullCard = chancecard[0];
+        for (int i = 0; i < chancecard.length; i++) {
+            chancecard[i] = chancecard[i + 1];
         }
-        public void deck() {
-            chanceDeck[0] = new Chance(1);
-            chanceDeck[1] = new Chance(2);
-            chanceDeck[2] = new Chance(3);
-            chanceDeck[3] = new Chance(4);
-            chanceDeck[4] = new Chance(5);
-            chanceDeck[5] = new Chance(6);
-            chanceDeck[6] = new Chance(7);
-            chanceDeck[7] = new Chance(8);
-            chanceDeck[8] = new Chance(9);
-            chanceDeck[9] = new Chance(10);
+        chancecard[chancecard.length - 1] = pullCard;
+        return pullCard;
+    }
 
-
+    public void shufflecard() {
+        int position;
+        String deck;
+        Random random = new Random();
+        for (int i = chancecard.length - 1; i > 0; i--) {
+            position = random.nextInt(i + 1);
+            deck = chancecard[position];
+            chancecard[position] = chancecard[i];
+            chancecard[i] = deck;
         }
 
-        /* public Chance getChanceCard(){
-            Random random=new Random();
-            int i = random.nextInt(chanceDeck.length);
-            return chanceDeck[i];
-        }*/
-        public Chance pullCard() {
-            Chance pull = chanceDeck[0];
-            for (int i = 0; i < chanceDeck.length; i++) {
-            chanceDeck[i]=chanceDeck[i+1];
-            }
-            chanceDeck[chanceDeck.length-1]=pull;
-            return pull;
-        }
+    }
 
-            public void shufflecard(){
-            int position;
-            Chance deck;
-            Random random = new Random();
-            for ( int i = chanceDeck.length-1;i>0;i--){
-                position = random.nextInt(i+1);
-                deck=chanceDeck[position];
-                chanceDeck[position]=chanceDeck[i];
-                chanceDeck[i] = deck;
-            }
+    @Override
+    public String toString() {
+        return "ChanceCard{" +
+                "chanceDeck=" + Arrays.toString(chancecard) +
+                '}';
 
-        }
-
-        @Override
-        public String toString() {
-            return "ChanceCard{" +
-                    "chanceDeck=" + Arrays.toString(chanceDeck) +
-                    '}';
-
-        }
-        }
-
-
+    }
+}
