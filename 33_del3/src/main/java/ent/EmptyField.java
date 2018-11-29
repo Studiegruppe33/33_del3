3ef2bf466684255bff02f9d2063b1c7d9bffc415
killemille <s185128@student.dtu.dk>
2018-11-29 13:36:45 +0100

    Rettelse i EmptyField-klassen

--- 33_del3/src/main/java/ent/EmptyField.java
+++ 33_del3/src/main/java/ent/EmptyField.java
@@ -2,11 +2,10 @@ package ent;
 
 public class EmptyField extends Field {
 
-
-
     public EmptyField(int number, String name, int result) {
         super(number, name,result);
     }
+
     @Override
     public void landOnField() {
         System.out.println(getName());
