4a7b6267586e84219906f0c11adad8f28465b6af
killemille <s185128@student.dtu.dk>
2018-11-30 11:11:20 +0100

    Ændring af txt-filnavn

---
+++ 33_del3/src/test/ent/AccountTest.java
@@ -0,0 +1,24 @@
+package ent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void addToScore() {
+        Account testScore = Player.getxAccount();
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addField() {
+    }
+}
