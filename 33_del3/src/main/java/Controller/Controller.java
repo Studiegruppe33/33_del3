8da5cfed832efc7e530501d3eea5091f98bd634c
Andreas Hyldahl <Andreas.hyldahl10@hotmail.com>
2018-11-30 11:19:17 +0100

    Sidste programmerings forsøg

--- 33_del3/src/main/java/Controller/Controller.java
+++ 33_del3/src/main/java/Controller/Controller.java
@@ -1,39 +1,39 @@
 package Controller;
 
 import ent.*;
+
 import java.util.Scanner;
 public class Controller {
     Account account = new Account();
     Player player = new Player();
-    Dice dice = new Dice();
-    Field field;
+    // håber ikke parametre har indflydelse?
+    Field field = new Field(1,"none",0);
+
+
+    Scanner scanner = new Scanner(System.in);
 
     public void startGame() {
-
-        ChanceCard chance= new ChanceCard();
-        chance.card();
-        System.out.println(chance);
-       /* GameBoard gameBoard=new GameBoard();
-        Player player = new Player();
-
 // bland chancekort metode kaldes først
-        Account dogAccount = new Account();
 
         player.amount();
 
         player.startScore();
+        while (player.getCarAccount().getScore() != 0 || player.getCatAccount().getScore() != 0 ||
+                player.getShipAccount().getScore() != 0 || player.getDogAccount().getScore() != 0) {
 
-        while (player.getxAccount().getScore() != 0) {
             player.playerTurn();
-            dice.ThrowDice();
+            System.out.println("Kast terningen");
+            scanner.nextLine();
             player.movePlayer();
             field.landOnField();
 
+
+
         }
+        System.out.println("Du har tabt");
         // metode der afgør hvem der vinder
 
 
-        System.out.println("Du har tabt");
-*/
     }
 }
+
