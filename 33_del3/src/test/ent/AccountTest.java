ec62d13ece7ca58a1caf6522c5560ea1514d7de9
killemille <s185128@student.dtu.dk>
2018-11-30 11:12:45 +0100

    Junit test af Account

---
+++ 33_del3/src/test/ent/AccountTest.java
@@ -0,0 +1,22 @@
+package ent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void addToScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setScore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addField() {
+    }
+}
