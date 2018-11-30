cba628d704b7cf40d620f2946cb847412a166955
killemille <s185128@student.dtu.dk>
2018-11-30 11:23:18 +0100

    Test1 - addToScore1

--- 33_del3/src/test/ent/AccountTest.java
+++ 33_del3/src/test/ent/AccountTest.java
@@ -1,11 +1,18 @@
 package ent;
 
+import sun.jvm.hotspot.utilities.Assert;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
+    //Test1
     @org.junit.jupiter.api.Test
     void addToScore() {
+        Player player = new Player();
+        Account testScore = player.getxAccount();
+        int result = 2;
+        assertEquals(2, testScore+result);
     }
 
     @org.junit.jupiter.api.Test
