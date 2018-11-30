345dc47688ef7c23a369727249cb288152f4a6f8
killemille <s185128@student.dtu.dk>
2018-11-30 11:25:55 +0100

    PlayerTest oprettet

---
+++ 33_del3/src/test/ent/PlayerTest.java
@@ -0,0 +1,20 @@
+package ent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void amount() {
+    }
+
+    @Test
+    void playerTurn() {
+    }
+
+    @Test
+    void startScore() {
+    }
+}
